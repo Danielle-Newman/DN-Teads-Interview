5a183bc90622cef6ec22d75c97eb37c259c54c00
Danielle-Newman <DanielleNewman2001@Gmail.com>
2025-05-13 10:41:53 +0200

    Removed erroneous semicolon

--- 2.2.3.sql
+++ 2.2.3.sql
@@ -8,6 +8,6 @@ SELECT
 FROM Advertisers a
 LEFT JOIN Campaigns c ON a.advertiser_id = c.advertiser_id
 LEFT JOIN AdMetrics am ON c.campaign_id = am.campaign_id
-WHERE a.advertiser_id IN ('ADV-111', 'ADV-222', 'ADV-333');
+WHERE a.advertiser_id IN ('ADV-111', 'ADV-222', 'ADV-333')
 GROUP BY a.advertiser_id, c.campaign_id, c.campaign_name
 ORDER BY total_impressions DESC, total_clicks DESC;
